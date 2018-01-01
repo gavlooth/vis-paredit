8ca19579124570bd09d2244316b325aeb4dff76e
Christos Chatzifountas <gavlooth@gmail.com>
2018-01-02 00:34:51 +0200

    :fire: Introduce good lpeg expression to burf and slurp

--- paredit.lua
+++ paredit.lua
@@ -5,22 +5,23 @@ local match = lpeg.match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
 local char_sexp_literals = P{"\\" * S('(){}[]"') }
+local char_literals = P{"\\" * (l.graph -  S("n\"\\"))}
 
-local str_pattern = ('"' *  ( ( P("\\\"") +    (1 -  S('"'))))^0 * '"' )^0
+local str_pattern = ('"' *  ( ( P("\\\"") +    (1 -  S('"'))))^0 * '"')
+local strings_and_chars = str_pattern + char_sexp_literals
 
-complete_balanced_sexp =   P{str_pattern +
-                            ("(" * ((1 - S("(){}[]")) * str_pattern + lpeg.V(1))^0 * ")") +
-                            ("[" * ((1 - S("(){}[]")) * str_pattern + lpeg.V(1))^0 * "]") +
-                            ("{" * ((1 - S("(){}[]")) * str_pattern + lpeg.V(1))^0 * "}") +
-                             (((l.graph -  S("(){}[]\"")) + char_sexp_literals)^0 * (S(" \n")^1)) }
+complete_balanced_sexp =   P{("(" * ((1 - S("(){}[]\"")) + strings_and_chars + lpeg.V(1))^0 * ")") +
+                             ("[" * ((1 - S("(){}[]\"")) + strings_and_chars + lpeg.V(1))^0 * "]") +
+                             ("{" * ((1 - S("(){}[]\"")) + strings_and_chars  + lpeg.V(1))^0 * "}") +
+                             (((l.graph -  S("(){}[]\"\\")) + char_literals))^1+ str_pattern^1  }
 
-                            match_sexp = {["("] = ")",
-              [")"] = "(",
-              ["["] = "]",
-              ["]"] = "[",
-              ["{"] = "}",
-              ["}"] = "{",
-              ["\""]="\"" }
+ match_sexp = {["("] = ")",
+               [")"] = "(",
+               ["["] = "]",
+               ["]"] = "[",
+               ["{"] = "}",
+               ["}"] = "{",
+               ["\""]="\"" }
 
 
 --This function returns a patternt that skips as many characters
@@ -36,19 +37,46 @@ end
 function match_next_sexp (pos) --pos + 1 ?
  local Range = {}
  local I = lpeg.Cp()
- local text = vis.win.file:content(pos,vis.win.file.size)
- local start, finish = match( S(" \n")^0 * I * complete_balanced_sexp * I, text  )
+ local text = vis.win.file:content(pos + 1,vis.win.file.size)
+ local start, finish = match(S(" \n")^0 * I  * complete_balanced_sexp * I, text  )
+ if start ~= nil then
  Range.start , Range.finish = pos + start, pos + finish
- print_two(Range.start,Range.finish)
+ end
+ -- print_two(Range.start,Range.finish)
+ return Range
+end
+
+
+
+function match_next_sexp_two(pos) --pos + 1 ?
+ local Range = {}
+ local I = lpeg.Cp()
+ local text = vis.win.file:content(pos + 1,vis.win.file.size)
+ local start, finish = match(S(" \n")^0 * I  * complete_balanced_sexp * I, text  )
+ if start ~= nil then
+ Range.start , Range.finish = pos + start, pos + finish
+ end
+ -- print_two(Range.start,Range.finish)
  return Range
 end
 
 function match_previus_sexp (starting_pos, pos, previus_sexp_pos)
-  local sexp_pos = match_next_sexp(starting_pos)
+  local sexp_pos = match_next_sexp (starting_pos)
     if sexp_pos.finish == nil then
       return match_previus_sexp (starting_pos + 1, pos,  previus_sexp_pos)
     elseif  sexp_pos.finish < pos then
-      return  advance_search_step (sexp_pos.finish, pos, sexp_pos  )
+      return  match_previus_sexp(sexp_pos.finish -1, pos, sexp_pos  )
+    else
+      return previus_sexp_pos
+  end
+end
+
+
+
+function match_previus_sexp_two (starting_pos, pos, previus_sexp_pos)
+  local sexp_pos = match_next_sexp (starting_pos)
+    if  sexp_pos.finish < pos then
+      return  match_previus_sexp_two(sexp_pos.finish, pos, sexp_pos  )
     else
       return previus_sexp_pos
   end
@@ -83,13 +111,15 @@ end
 
 function slurp_sexp_forward ()
  local file, pos = vis.win.file,  vis.win.selection.pos
- local sexp_pos = match_next_sexp (pos)
+ local sexp_pos = match_next_sexp(pos)
+move_sexp(pos, sexp_pos.finish)
 end
 
 function slurp_sexp_backwards ()
  local file, pos = vis.win.file,  vis.win.selection.pos
- local sexp_range = match_previus_sexp(0, pos)
- move_sexp(pos, sexp_range.start - 1)
+ local sexp_range = match_previus_sexp(0, pos +  1)
+
+ move_sexp(pos, sexp_range.start)
 end
 
 
